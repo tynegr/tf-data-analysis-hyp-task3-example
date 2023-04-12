2347280d9043eb4cff7813fae7d5569bfda56365
tynegr <122130536+tynegr@users.noreply.github.com>
2023-04-12 23:58:07 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,15 +1,14 @@
 import pandas as pd
 import numpy as np
-#import scipy.stats as stats
 from scipy.stats import mannwhitneyu
 
 chat_id = 462449141 # Ваш chat ID, не меняйте название переменной
 
-def solution(x,y) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    alpha = 0.01
-    #_,pv = stats.ttest_ind(x, y, equal_var=False)
-    pv = mannwhitneyu(x, y, alternative='greater', method='asymptotic')
-    if pv.pvalue <= alpha:
+SGN_LVL = 0.01
+
+def solution(x: np.array, y: np.array) -> bool:
+    pval = mannwhitneyu(x, y, alternative="less").pvalue
+    if pval < SGN_LVL:
         return True
-    else:
-        return False
+    # true: выборки не равны
+    return False
