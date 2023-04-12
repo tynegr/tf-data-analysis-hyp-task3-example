c6d50aad5ab16918b783d9766f2fa6b53ef8c2f5
tynegr <122130536+tynegr@users.noreply.github.com>
2023-04-13 00:08:23 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,13 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import mannwhitneyu
+from scipy.stats import permutation_test
 
 chat_id = 462449141 # Ваш chat ID, не меняйте название переменной
 
-SGN_LVL = 0.01
-
-def solution(x: np.array, y: np.array) -> bool:
-    pval = mannwhitneyu(x, y, alternative="less").pvalue
-    if pval < SGN_LVL:
-        return True
-    # true: выборки не равны
-    return False
+def solution(x: np.array, y: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+    alpha = 0.01
+    znach = permutation_test((x, y), lambda x, y, axis: np.mean(x, axis=axis) - np.mean(y, axis=axis), 
+                 vectorized=True, 
+                 n_resamples=5000,
+                 alternative='greater').pvalue < alpha
+    return znach # Ваш ответ, True или False
