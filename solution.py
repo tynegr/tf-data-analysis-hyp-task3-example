272e2c22c9396a194a23c00ffbadee8fae8e409d
tynegr <122130536+tynegr@users.noreply.github.com>
2023-04-12 23:44:27 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import numpy as np
+#import scipy.stats as stats
+from scipy.stats import mannwhitneyu
 
+chat_id = 462449141 # Ваш chat ID, не меняйте название переменной
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(x,y) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+    alpha = 0.01
+    #_,pv = stats.ttest_ind(x, y, equal_var=False)
+    pv = mannwhitneyu(x, y, alternative='greater', method='asymptotic')
+    if pv.pvalue <= alpha:
+        return True
+    else:
+        return False
