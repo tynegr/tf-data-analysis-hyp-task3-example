e79136e5889639818cfdf009ccae33e03a1a270f
tynegr <122130536+tynegr@users.noreply.github.com>
2023-04-13 20:01:43 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -3,7 +3,7 @@ import numpy as np
 import scipy.stats as stats
 from scipy.stats import permutation_test
 
-chat_id = 298754188 # Ваш chat ID, не меняйте название переменной
+chat_id = 462449141 # Ваш chat ID, не меняйте название переменной
 
 def solution(x, y) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     return stats.ttest_ind(x, y)[1] < 0.01 # Ваш ответ, True или False
